f9ad4307221788e1ccdbe50d91d5ca4b18d97fc9
jjmjyrihs <1104137128@gm.kuas.edu.tw>
2017-05-31 00:22:12 +0800

    加入Order

---
+++ ASP/eSaleModel/Order.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace eSaleModel
+{
+    public class Order
+    {
+        /// <summary>
+        /// 客戶代號
+        /// </summary>
+        [DisplayName("客戶編號")]
+        [MaxLength(3)]
+        public string CustomerID { get; set; }
+
+        /// <summary>
+        /// 客戶名稱
+        /// </summary>
+        [DisplayName("客戶名稱")]
+        public string CustName { get; set; }
+
+        /// <summary>
+        /// 員工編號
+        /// </summary>    
+        [DisplayName("員工編號")]
+        public string EmployeeID { get; set; }
+
+        /// <summary>
+        /// 員工名稱
+        /// </summary>
+        [DisplayName("員工名稱")]
+        public string EmpName { get; set; }
+
+        /// <summary>
+        /// 貨物運費
+        /// </summary>
+        [DisplayName("貨物運費")]
+        public string Freight { get; set; }
+
+        /// <summary>
+        /// 訂單日期
+        /// </summary>
+        [DisplayName("訂單日期")]
+        public string OrderDate { get; set; }
+        /// <summary>
+        /// 訂單編號
+        /// </summary>
+        [DisplayName("訂單編號")]
+        [Required()]
+        public string OrderID { get; set; }
+
+        /// <summary>
+        /// 需要日期
+        /// </summary>
+        [DisplayName("需要日期")]
+        public string RequiredDate { get; set; }
+
+        /// <summary>
+        /// 出貨地址
+        /// </summary>
+        [DisplayName("出貨地址")]
+        public string ShipAddress { get; set; }
+
+        /// <summary>
+        /// 出貨城市
+        /// </summary>
+        [DisplayName("出貨城市")]
+        public string ShipCity { get; set; }
+
+        /// <summary>
+        /// 出貨國家
+        /// </summary>
+        [DisplayName("出貨國家")]
+        public string ShipCountry { get; set; }
+
+        /// <summary>
+        /// 出貨日期
+        /// </summary>
+        [DisplayName("出貨日期")]
+        public string ShippedDate { get; set; }
+
+        /// <summary>
+        /// 出貨編號
+        /// </summary>
+        [DisplayName("出貨編號")]
+        public string ShipperId { get; set; }
+
+        /// <summary>
+        /// 郵政區號
+        /// </summary>
+        [DisplayName("郵政區號")]
+        public string ShipPostalCode { get; set; }
+
+        /// <summary>
+        /// 出貨地區
+        /// </summary>
+        [DisplayName("出貨地區")]
+        public string ShipRegion { get; set; }
+
+        /// <summary>
+        /// 公司名稱
+        /// </summary>
+        [DisplayName("公司名稱")]
+        public string CpyName { get; set; }
+
+        
+        
+    }
+}
---
+++ ASP/eSaleService/OrderService.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace eSaleService
+{
+    public class OrderService
+    {
+        /// <summary>
+        /// 取得DB連線
+        /// </summary>
+        /// <returns></returns>
+
+
+
+        public List<eSaleModel.Order> Inquire(eSaleModel.Order AllData)
+        {
+            string SqlData = "Select OrderID, CustName, OrderDate, ShippedDate ";
+            string Where = "Where ";
+            if (AllData.OrderID != null)
+            {
+                Where += "a.OrderID = " + AllData.OrderID + " and ";
+            }
+            if (AllData.CustName != null)
+            {
+                Where += "CustName = 'Customer IBVRG'" + " and ";
+            }
+            if (!AllData.EmpName.Equals("null"))
+            {
+                Where += "a.EmployeeID = " + AllData.EmpName + " and ";
+            }
+            if (!AllData.CpyName.Equals("null"))
+            {
+                Where += "d.ShipperID = " + AllData.CpyName + " and ";
+            }
+            if (AllData.OrderDate != null)
+            {
+                Where += "OrderDate = '" + AllData.OrderDate.Substring(0, 10) + "' and ";
+
+            }
+            if (AllData.RequiredDate != null)
+            {
+                Where += "RequiredDate = '" + AllData.RequiredDate.Substring(0, 10) + "' and ";
+            }
+            if (AllData.ShippedDate != null)
+            {
+                Where += "ShippedDate = '" + AllData.ShippedDate.Substring(0, 10) + "' and ";
+            }
+            if (Where.Equals("Where "))
+            {
+                Where = "";
+            }
+            List<eSaleModel.Order> Data = DoData(SqlData, (Where.Substring(0, Where.Length - 5)), AllData);
+            return Data;
+        }
+
+        public void Delect(eSaleModel.Order AllData)
+        {
+            string SqlData = "Delect ";
+            string Where = "a.OrderID = " + AllData;
+            DoData(SqlData, Where, AllData);
+        }
+
+
+        public List<eSaleModel.Order> DoData(string SqlData, string Where, eSaleModel.Order AllData)
+        {
+            DataTable dt = new DataTable();
+            string Sql = @SqlData + "from Sales.Orders a inner join Sales.Customers b " +
+                                  "on b.CustomerID = a.CustomerID " +
+                                  "inner join HR.Employees c " +
+                                  "on c.EmployeeID = a.EmployeeID " +
+                                  "inner join Sales.Shippers d " +
+                                  "on d.ShipperID = a.ShipperID " +
+                                  Where;
+
+
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DBconn"].ConnectionString);
+            using (conn)
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(Sql, conn);
+
+                SqlDataAdapter sqlAdapter = new SqlDataAdapter(cmd);
+                sqlAdapter.Fill(dt);
+                conn.Close();
+            }
+            List<eSaleModel.Order> Data = new List<eSaleModel.Order>();
+            Data = MapOrderDataToList(dt);
+            return Data;
+
+        }
+
+
+
+
+        private List<eSaleModel.Order> MapOrderDataToList(DataTable orderData)
+        {
+
+            List<eSaleModel.Order> result = new List<eSaleModel.Order>();
+
+
+            foreach (DataRow row in orderData.Rows)
+            {
+                result.Add(
+                    new eSaleModel.Order
+                    {
+                        OrderID = row["OrderID"].ToString(),
+                        CustName = row["CustName"].ToString(),
+                        OrderDate = row["OrderDate"].ToString(),
+                        ShippedDate = row["ShippedDate"].ToString()
+                    }
+                );
+            }
+
+            return result;
+        }
+
+    }
+}
