a7b88d1ca52e17994365cbf8544f6703f13ef502
jjmjyrihs <1104137128@gm.kuas.edu.tw>
2018-01-11 15:55:36 +0800

    Add files via upload

---
+++ Data.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Data
+    {
+        public string OrderId { set; get; }
+        public string CustName { set; get; }
+        public string EmpName { set; get; }
+        public string CpyName { set; get; }
+
+        public string OrderDate { set; get; }
+        public string RequiredDate { set; get; }
+        public string ShippedDate { set; get; }
+
+        public string ShipAddress { set; get; }
+        public string ShipCity { set; get; }
+        public string ShipRegion { set; get; }
+        public string ShipPostalCode { set; get; }
+        public string ShipCountry { set; get; }
+    }
+}
